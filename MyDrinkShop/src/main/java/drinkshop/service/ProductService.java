4c298a395aefa6d05ab8bfe9b6116691ece2d3a2
qRares <antoniu.bota@stud.ubbcluj.ro>
2026-04-01 11:34:16 +0300

    Update ProductService.java

--- MyDrinkShop/src/main/java/drinkshop/service/ProductService.java
+++ MyDrinkShop/src/main/java/drinkshop/service/ProductService.java
@@ -2,6 +2,7 @@ package drinkshop.service;
 
 import drinkshop.domain.*;
 import drinkshop.repository.Repository;
+import drinkshop.service.validator.ProductValidator;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,17 +10,20 @@ import java.util.stream.Collectors;
 public class ProductService {
 
     private final Repository<Integer, Product> productRepo;
+    private final ProductValidator validator = new ProductValidator();
 
     public ProductService(Repository<Integer, Product> productRepo) {
         this.productRepo = productRepo;
     }
 
     public void addProduct(Product p) {
+        validator.validate(p);
         productRepo.save(p);
     }
 
     public void updateProduct(int id, String name, double price, CategorieBautura categorie, TipBautura tip) {
         Product updated = new Product(id, name, price, categorie, tip);
+        validator.validate(updated);
         productRepo.update(updated);
     }
 
@@ -28,13 +32,6 @@ public class ProductService {
     }
 
     public List<Product> getAllProducts() {
-//        Iterable<Product> it=productRepo.findAll();
-//        ArrayList<Product> products=new ArrayList<>();
-//        it.forEach(products::add);
-//        return products;
-
-//        return StreamSupport.stream(productRepo.findAll().spliterator(), false)
-//                    .collect(Collectors.toList());
         return productRepo.findAll();
     }
 
